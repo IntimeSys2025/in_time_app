d59817bcf571cf118d582405ed51253e119e1645
asmaa <asmaaelbanna21@gmail.com>
2025-11-10 14:17:03 +0200

    notification

--- lib/features/profile/presentation/screens/notification_screen.dart
+++ lib/features/profile/presentation/screens/notification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:in_time_app/core/helpers/extension.dart';
+import 'package:in_time_app/core/utils/app_colors.dart';
 import 'package:in_time_app/core/utils/app_font_size.dart';
 
 import '../widget/notification_item.dart';
@@ -11,6 +12,7 @@ class NotificationsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.white,
         title: const Text('Notifications'),
         centerTitle: true,
         leading: IconButton(
@@ -19,72 +21,108 @@ class NotificationsScreen extends StatelessWidget {
         ),
       ),
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: ListView(
-              // padding: EdgeInsets.symmetric(horizontal: 16.0),
-              children: [
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
-                  child: Text(
-                    'Newest',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: AppFontSize.fontSize18),
-                  ),
-                ),
-                const NotificationItem(
-                    title: 'Reminder! Get ready for your appointment at 9am',
-                    time: 'Just Now',
-
-                    icon: Icons.calendar_today),
-                10.heightSpace,
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
-                  child: Text(
-                    'Oldest',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: AppFontSize.fontSize18),
-                  ),
-                ),
-                const NotificationItem(
-                    title: 'Payment at Famous Barber was successful!',
-                    time: '08:23 AM',
-                    date: '12/2/2024',
-                    icon: Icons.wallet),
-                10.heightSpace,
-                const NotificationItem(
-                    title: 'Reminder! Get ready for your appointment at 9am',
-                    time: 'Just Now',
-                    icon: Icons.calendar_today),
-                10.heightSpace,
-                const NotificationItem(
-                    title: 'Reminder! Get ready for your appointment at 9am',
-                    time: 'Just Now',
-                    icon: Icons.calendar_today),
-                10.heightSpace,
-                const NotificationItem(
-                    title: 'Reminder! Get ready for your appointment at 9am',
-                    time: 'Just Now',
-                    icon: Icons.calendar_today),
-                10.heightSpace,
-                const NotificationItem(
-                    title: 'Reminder! Get ready for your appointment at 9am',
-                    time: 'Just Now',
-                    icon: Icons.calendar_today),
-                10.heightSpace,
-                const NotificationItem(
-                    title: 'Reminder! Get ready for your appointment at 9am',
-                    time: 'Just Now',
-                    icon: Icons.calendar_today),
-                10.heightSpace,
-                const NotificationItem(
-                    title: 'Reminder! Get ready for your appointment at 9am',
-                    time: 'Just Now',
-                    icon: Icons.calendar_today),
-              ],
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
+            child: Text(
+              'Newest',
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: AppFontSize.fontSize18),
             ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return NotificationItem(
+                    isEven: index.isEven,
+                    title: const Text.rich(
+                      TextSpan(
+                          text: 'Reminder! ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
+                          children: [
+                            TextSpan(
+                                text: 'Get ready for your appointment at 9am',
+                                style: TextStyle(fontWeight: FontWeight.normal))
+                          ]),
+                    ),
+                    // title: 'Reminder! Get ready for your appointment at 9am',
+                    time: 'Just Now',
+                    icon: index.isEven
+                        ? Icons.calendar_today
+                        : Icons.monetization_on_outlined);
+              },
+            ),
+
+            // ListView(
+            //   // padding: EdgeInsets.symmetric(horizontal: 16.0),
+            //   children: [
+            //     const Padding(
+            //       padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
+            //       child: Text(
+            //         'Newest',
+            //         style: TextStyle(
+            //             fontWeight: FontWeight.w600,
+            //             fontSize: AppFontSize.fontSize18),
+            //       ),
+            //     ),
+            //     const NotificationItem(
+            //         title: 'Reminder! Get ready for your appointment at 9am',
+            //         time: 'Just Now',
+            //
+            //         icon: Icons.calendar_today),
+            //     10.heightSpace,
+            //     const Padding(
+            //       padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
+            //       child: Text(
+            //         'Oldest',
+            //         style: TextStyle(
+            //             fontWeight: FontWeight.w600,
+            //             fontSize: AppFontSize.fontSize18),
+            //       ),
+            //     ),
+            //     const NotificationItem(
+            //         title: 'Payment at Famous Barber was successful!',
+            //         time: '08:23 AM',
+            //         date: '12/2/2024',
+            //         icon: Icons.wallet),
+            //     10.heightSpace,
+            //     const NotificationItem(
+            //         title: 'Reminder! Get ready for your appointment at 9am',
+            //         time: 'Just Now',
+            //         icon: Icons.calendar_today),
+            //     10.heightSpace,
+            //     const NotificationItem(
+            //         title: 'Reminder! Get ready for your appointment at 9am',
+            //         time: 'Just Now',
+            //         icon: Icons.calendar_today),
+            //     10.heightSpace,
+            //     const NotificationItem(
+            //         title: 'Reminder! Get ready for your appointment at 9am',
+            //         time: 'Just Now',
+            //         icon: Icons.calendar_today),
+            //     10.heightSpace,
+            //     const NotificationItem(
+            //         title: 'Reminder! Get ready for your appointment at 9am',
+            //         time: 'Just Now',
+            //         icon: Icons.calendar_today),
+            //     10.heightSpace,
+            //     const NotificationItem(
+            //         title: 'Reminder! Get ready for your appointment at 9am',
+            //         time: 'Just Now',
+            //         icon: Icons.calendar_today),
+            //     10.heightSpace,
+            //     const NotificationItem(
+            //         title: 'Reminder! Get ready for your appointment at 9am',
+            //         time: 'Just Now',
+            //         icon: Icons.calendar_today),
+            //   ],
+            // ),
           ),
         ],
       ),
--- lib/features/profile/presentation/widget/notification_item.dart
+++ lib/features/profile/presentation/widget/notification_item.dart
@@ -2,34 +2,39 @@ import 'package:flutter/material.dart';
 import '../../../../core/utils/app_colors.dart';
 
 class NotificationItem extends StatelessWidget {
-  final String title;
+  final bool isEven;
+  final Widget title;
   final IconData icon;
   final String? date;
   final String time;
   const NotificationItem(
-      {super.key, required this.title, required this.icon, required this.time,  this.date});
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.time,
+      this.date,
+      required this.isEven});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppColors.kLightGreen.withOpacity(0.7),
-      child:  ListTile(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      color: isEven ? AppColors.kLightGreen.withOpacity(0.7):AppColors.white,
+      child: ListTile(
         leading: CircleAvatar(
           backgroundColor: AppColors.kGreenButton,
           child: Icon(icon, color: Colors.white),
         ),
         trailing: Column(
-           crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
-            if(date != null)
-              Text(date!),
+            if (date != null) Text(date!),
             Text(time),
-
           ],
         ),
-        title: Text(title),
+        title: title,
         // subtitle: Text(time),
       ),
     );
